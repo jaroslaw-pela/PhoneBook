13736edd982eafb8b4974701fb18b2aa04e3aaa3
Jarek <jaroslaw.pela@wp.pl>
2023-08-19 10:58:17 +0200

    Add project files.

---
+++ Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class Contact
+    {
+        public Contact(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PhineBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
